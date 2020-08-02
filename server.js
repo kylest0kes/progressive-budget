10b96955284cab1a0744fb14a860128cd97dfe16
kylest0kes <kylestokes1551@gmail.com>
2020-08-01 21:12:45 -0400

    heroku fix

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
 
 const PORT = 3000;
 
@@ -16,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget");
 
 // routes
 app.use(require("./routes/api.js"));
